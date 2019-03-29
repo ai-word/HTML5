b819f9e9d4858cb851cad0d71e1d2810922f6d82
刘玉刚 <huayue430@126.com>
2019-03-29 09:16:50 +0800

    更新vue项目
    更新vue项目

---
+++ HTML5-Vue项目/ImageHandle/miniPaint/webpack.config.js
@@ -0,0 +1,57 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: [
+		'babel-polyfill',
+		'./src/js/main.js',
+	],
+	output: {
+		filename: 'dist/bundle.js',
+	},
+	resolve: {
+		extensions: ['.js', '.css'],
+		alias: {
+			Utilities: path.resolve(__dirname, './../node_modules/')
+		}
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {minimize: true, url: false}
+					}
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+				loader: 'babel-loader',
+					options: {
+					presets: ['env']
+					}
+				}
+			},
+		]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+		}),
+		new webpack.DefinePlugin({
+			VERSION: JSON.stringify(require("./package.json").version)
+		}),
+	],
+	devtool: "cheap-module-source-map",
+	devServer: {
+		contentBase: "./",
+		compress: true,
+	}
+};
